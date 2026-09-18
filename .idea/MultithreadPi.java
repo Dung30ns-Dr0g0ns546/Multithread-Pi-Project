cb413ef2133f440f3d1c7127303bbc08b68f7811
Dung30ns-Dr0g0ns546 <qaden.childs@gmail.com>
2026-09-18 09:32:12 -0400

    commit

--- .idea/MultithreadPi.java
+++ .idea/MultithreadPi.java
@@ -7,3 +7,11 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+
+public class MultithreadPi {
+    private static final long totalPoints = 500_000_000;
+
+    public static void main(String[] args) throw Exception {
+
+    }
+}
